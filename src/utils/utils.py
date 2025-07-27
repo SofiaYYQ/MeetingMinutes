3d8b083252aa413f8585a6c80936823f0b07bd52
sofia <sofiayyq@gmail.com>
2025-07-27 13:56:39 +0200

    deleted unused methods

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -3,63 +3,10 @@ import datetime
 import json
 import re
 from typing import Dict, List
-from config_loader.models import FullConfig, MetadataConfig
+from config_loader.models import MetadataConfig
 
 
 class Utils():
-    @staticmethod
-    def get_analysis_output_name(prefix: str, full_config: FullConfig) -> str:
-        model_name = full_config.app.general.llm.model_name
-        get_metadata = full_config.app.pre_retrieval.get_metadata
-        use_chunks = full_config.app.pre_retrieval.use_chunks
-        chunks_config = full_config.app.pre_retrieval.chunks_config
-        response_mode = full_config.app.post_retrieval.response_mode
-    
-        current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
-
-        model_name_in_file = model_name.replace(":", ".")
-
-        chunks_indicator = f"{chunks_config.chunk_size}_{chunks_config.chunk_overlap}"
-        
-        parts = []
-        parts.append(prefix)
-        parts.append(f"-{model_name_in_file}")
-        if get_metadata:
-            parts.append(f"-metadata")
-        if use_chunks:
-            parts.append(f"-{chunks_indicator}")
-        parts.append(f"-{response_mode.value}")
-        parts.append(f"-{current_datetime}")
-        
-        # filename = f"{prefix}-{model_name_in_file}-{metadata_indicator}{chunks_indicator}-{response_mode.value}-{current_datetime}"
-        filename = "".join(parts)
-        return filename
-    
-    @staticmethod
-    def get_specific_chromadb_name(full_config: FullConfig) -> str:
-        get_metadata = full_config.app.pre_retrieval.get_metadata
-        use_chunks = full_config.app.pre_retrieval.use_chunks
-        chunks_config = full_config.app.pre_retrieval.chunks_config
-        
-        chunks_indicator = f"{chunks_config.chunk_size}_{chunks_config.chunk_overlap}"
-        
-        parts = []
-        if get_metadata:
-            parts.append(f"-metadata")
-        if use_chunks:
-            parts.append(f"-{chunks_indicator}")
-
-        suffix = "".join(parts)
-
-        database_path = full_config.app.general.db.database_path
-        if '/' in database_path:
-            separator_index = database_path.find('/')
-            first_part = database_path[:separator_index]
-            rest_of_path = database_path[separator_index:]
-            return f"{first_part}{suffix}{rest_of_path}"
-        else:
-            return database_path
-
     @staticmethod
     def get_testing_analysis_output_name(prefix: str) -> str:
         current_datetime = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
